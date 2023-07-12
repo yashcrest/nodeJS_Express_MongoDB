3b4fd2fbc6afb09c3010f9383baf6e44017687c8
yashcrest <yashcrest@gmail.com>
2023-07-12 13:13:15 +0930

    commit

--- app.js
+++ app.js
@@ -61,4 +61,4 @@ app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)) 
