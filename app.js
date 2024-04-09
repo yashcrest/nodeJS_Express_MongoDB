fbfcaf15158666cfbb2c40d1a45911c14ad03cd0
Yash Shrestha <yashcrest@gmail.com>
2024-04-09 15:22:36 +1000

    removing typos

--- app.js
+++ app.js
@@ -1,64 +1,76 @@
-const path = require('path')
-const express = require('express')
-const mongoose  = require('mongoose')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const passport = require('passport')
-const session = require('express-session')
-const {engine} = require('express-handlebars')
-const MongoStore = require('connect-mongo');
-const connectDB = require('./config/db')
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+const { engine } = require("express-handlebars");
+const MongoStore = require("connect-mongo");
+const connectDB = require("./config/db");
 
 //load config
-dotenv.config({path: './config/config.env' })
+dotenv.config({ path: "./config/config.env" });
 
-//Passport config 
-require('./config/passport')(passport)
+//Passport config
+require("./config/passport")(passport);
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 //Body parse
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //Logging
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //Handlebars Helpers
-const {formatDate,stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate } = require("./helpers/hbs");
 
 //Handlebars
-app.engine('.hbs' , engine({ helpers :{
-    formatDate,
-    stripTags,
-    truncate
-} ,defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine(
+  ".hbs",
+  engine({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
 
 //Sessions
-app.use(session({
-    secret : 'keyboard cat',
-    resave : false,
-    saveUninitialized : false,
-    store: MongoStore.create({mongoUrl : process.env.MONGO_URI,}),
-}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
 
 //Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
-app.use('/', require('./routes/index'))
-app.use('/auth', require('./routes/auth'))
-app.use('/stories', require('./routes/stories'))
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)) 
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`)
+);
